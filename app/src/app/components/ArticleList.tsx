42329b1c6d4a59b859041873fe9d48f1c95aded9
rstar <rstar0823@icloud.com>
2024-06-08 14:14:33 +0900

    feat: 記事一覧をAPIから取得するよう変更

--- app/src/app/components/ArticleList.tsx
+++ app/src/app/components/ArticleList.tsx
@@ -1,49 +1,47 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Article } from "../types";
 
-const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1345691/1000x500?sig=1"
-            alt=""
-            width={1280}
-            height={300}
-          ></Image>
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pd-4 font-bold">Technology</Link>
-          <Link href="#" className="text-slate-900 text-3x1 font-bold hover:text-gray-700 pb-4">Study Next.js</Link>
-          <p className="text-sm pb-3 text-slate-900">Published on 2024/06/05</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laboriosam hic illum veniam quos quis eligendi accusantium ullam aliquid, facere nobis laudantium totam exercitationem culpa et ex sed, magnam vero?
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">続きを読む</Link>
-        </div>
-      </article>
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://source.unsplash.com/collection/1345691/1000x500?sig=2"
-            alt=""
-            width={1280}
-            height={300}
-          ></Image>
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pd-4 font-bold">Technology</Link>
-          <Link href="#" className="text-slate-900 text-3x1 font-bold hover:text-gray-700 pb-4">Study Next.js</Link>
-          <p className="text-sm pb-3 text-slate-900">Published on 2024/06/05</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laboriosam hic illum veniam quos quis eligendi accusantium ullam aliquid, facere nobis laudantium totam exercitationem culpa et ex sed, magnam vero?
-          </Link>
-          <Link href="#" className="text-pink-800 hover:text-black">続きを読む</Link>
-        </div>
-      </article>
-
+      {articles.map((article) => {
+        return (
+          <article className="shadow my-4 flex flex-col" key={article.id}>
+            <Link href={`articles/${article.id}`} className="hover:opacity-75">
+              <Image
+                src="https://source.unsplash.com/collection/1345691/1000x500?sig=2"
+                alt=""
+                width={1280}
+                height={300}
+              ></Image>
+            </Link>
+            <div className="bg-white flex flex-col justify-start p-6">
+              <Link href="#" className="text-blue-700 pd-4 font-bold">
+                Technology
+              </Link>
+              <Link
+                href={`articles/${article.id}`}
+                className="text-slate-900 text-3x1 font-bold hover:text-gray-700 pb-4"
+              >
+                {article.title}
+              </Link>
+              <p className="text-sm pb-3 text-slate-900">{article.createdAt}</p>
+              <Link href={`articles/${article.id}`} className="text-slate-900 pb-6">
+                {article.content}
+              </Link>
+              <Link href={`articles/${article.id}`} className="text-pink-800 hover:text-black">
+                続きを読む
+              </Link>
+            </div>
+          </article>
+        );
+      })}
     </div>
   );
 };
